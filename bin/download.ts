7d672dd04ea280b3624a93fc6621bd82f300eda5
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2022-12-04 12:20:12 -0500

    update download script

--- bin/download.ts
+++ bin/download.ts
@@ -1,12 +1,12 @@
-import * as dotenv from "dotenv";
-import * as fs from "fs";
+import { readFile, writeFile } from "fs/promises";
 
 import axios from "axios";
+import { config } from "dotenv";
 import getGeneratorOptions from "./lib";
 
-dotenv.config();
+config();
 
-const basePath = process.env.BASE_PATH || "https://entrust.us.trustedauth.com";
+const basePath = "https://entrust.us.trustedauth.com";
 
 const DOC_PATH = `${basePath}/documentation/apiDocs`;
 const VERSION_KEY = "npmVersion";
@@ -18,69 +18,76 @@ interface Swagger {
 }
 
 /**
+ * Formats a version so that the patch version is properly applied.
+ *
+ * 5.5 -> 5.5.0
+ * 5.5.1 -> 5.5.1
+ *
+ * @param version The version read from the Swagger file.
+ * @returns The properly formatted version.
+ */
+const formatVersion = (version: string) => {
+  const [major, minor, patch = "0"] = version.split(".");
+
+  if (!major || !minor) {
+    console.error("Invalid IDaaS version found", version);
+    process.exit(1);
+  }
+
+  return [major, minor, patch].join(".");
+};
+
+/**
+ * Download and save Swagger file.
+ */
+const downloadFile = async (type: "auth" | "admin") => {
+  const { config: configFile, input: file } = getGeneratorOptions(type);
+
+  const URL = `${DOC_PATH}/${file}`;
+
+  console.log("Downloading", file, "from", URL);
+
+  const response = await axios.get(URL, { responseType: "json" });
+
+  const swagger = response.data as Swagger;
+
+  if (!swagger.info.version) {
+    console.error(
+      "Version not found in Swagger file.",
+      JSON.stringify(swagger, null, 2)
+    );
+    process.exit(1);
+  }
+
+  console.log("Writing", file);
+
+  await writeFile(file, JSON.stringify(swagger, null, 2), {
+    encoding: "utf-8",
+  });
+
+  console.log("Reading", configFile);
+
+  const configStr = await readFile(configFile, { encoding: "utf-8" });
+
+  const config = JSON.parse(configStr);
+
+  const version = formatVersion(swagger.info.version);
+
+  config[VERSION_KEY] = version;
+
+  console.log("Saving", configFile);
+
+  await writeFile(configFile, JSON.stringify(config, null, 2), {
+    encoding: "utf-8",
+  });
+
+  console.log("Saved.");
+};
+
+/**
  * Main function.
  */
 (async () => {
-  /**
-   * Formats a version so that the patch version is properly applied.
-   *
-   * 5.5 -> 5.5.0
-   * 5.5.1 -> 5.5.1
-   *
-   * @param version The version read from the Swagger file.
-   * @returns The properly formatted version.
-   */
-  const formatVersion = (version: string) => {
-    const [major, minor, patch = "0"] = version.split(".");
-
-    if (!major || !minor) {
-      console.error("Invalid IDaaS version found", version);
-      process.exit(1);
-    }
-
-    return [major, minor, patch].join(".");
-  };
-
-  /**
-   * Download and save Swagger file.
-   * @param file The name of the swagger file.
-   */
-  const downloadFile = async (type: "auth" | "admin") => {
-    const { config: configFile, input: file } = getGeneratorOptions(type);
-
-    const URL = `${DOC_PATH}/${file}`;
-
-    console.log("Downloading", file, "from", URL);
-
-    const response = await axios.get(URL, { responseType: "json" });
-
-    const swagger = response.data as Swagger;
-
-    console.log("Writing", file);
-
-    fs.writeFileSync(file, JSON.stringify(swagger, null, 2), {
-      encoding: "utf-8",
-    });
-
-    console.log("Reading", configFile);
-
-    const configStr = fs.readFileSync(configFile, { encoding: "utf-8" });
-
-    const config = JSON.parse(configStr);
-
-    const version = formatVersion(swagger.info.version);
-
-    config[VERSION_KEY] = version;
-
-    console.log("Saving", configFile);
-
-    fs.writeFileSync(configFile, JSON.stringify(config, null, 2), {
-      encoding: "utf-8",
-    });
-
-    console.log("Saved.");
-  };
-
   await downloadFile("admin");
   await downloadFile("auth");
 })();
